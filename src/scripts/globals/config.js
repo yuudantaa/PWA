6e1bd5423db75e633f31a9d0152ea8a5de2d54c5
yuudantaa <yeudantamahardika@gmail.com>
2024-11-13 10:16:31 +0700

    commit

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,12 @@
+const CONFIG = {
+    KEY: '744e68e64cd6fec7cd34016d14f31160',
+    BASE_URL: 'https://api.themoviedb.org/3/',
+    BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+    DEFAULT_LANGUAGE: 'en-us',
+    CACHE_NAME: 'MovieCatalogue-V1',
+    DATABASE_NAME: 'movie-catalogue-database',
+    DATABASE_VERSION: 1,
+    OBJECT_STORE_NAME: 'movies',
+  };
+   
+  export default CONFIG;
